adf0110de3f0fa70f0cff0ec74d966f0c4d5551b
wosuke <bloomxxx@me.com>
2019-01-30 20:27:41 +0900

    commit fix

--- static/scr/common.js
+++ static/scr/common.js
@@ -1,46 +1,128 @@
-let counter = 1;
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// indexedDB
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let db,
+    dbName    = 'sampleDB',
+    dbVersion = '1',
+    storeName = 'counter',
+    count_a   = 0,
+    count_b   = 0,
+    count_c   = 0;
+
+// DB名を指定して接続
+let openReq = indexedDB.open(dbName, dbVersion);
+
+// 接続に失敗
+openReq.onerror = function (event) {
+  console.log('接続失敗');
+}
+
+// DBのバージョン更新（DBの新規作成も含む）時のみ実行
+openReq.onupgradeneeded = function (event) {
+        db          = event.target.result;
+  const objectStore = db.createObjectStore(storeName, {keyPath : 'id'});
+
+  objectStore.createIndex(    "id",     "id", { unique: true  });
+  objectStore.createIndex("action", "action", { unique: false });
+  objectStore.createIndex(   "buy",    "buy", { unique: false });
+  objectStore.createIndex(  "coin",   "coin", { unique: false });
+
+  console.log('DB更新');
+}
+
+// onupgradeneededの後に実行。更新がない場合はこれだけ実行
+openReq.onsuccess = function (event) {
+
+      db       = event.target.result;
+  let trans_g  = db.transaction(storeName, 'readonly'),
+      store_g  = trans_g.objectStore(storeName),
+      getReq_g = store_g.get(1);
+
+  getReq_g.onsuccess = function (event) {
+
+    console.log(event.target.result);
+    if (typeof event.target.result === 'undefined') {
+      count_a = 0;
+      count_b = 0;
+      count_c = 0;
+    } else {
+      count_a = event.target.result.action;
+      count_b = event.target.result.buy;
+      count_c = event.target.result.coin;
+    }
+
+    let trans  = db.transaction(storeName, "readwrite"),
+        store  = trans.objectStore(storeName),
+        putReq = store.put({
+          id: 1,
+          action: count_a,
+          buy:    count_b,
+          coin:   count_c
+        });
+
+    putReq.onsuccess = function (event) {
+      $('#add_a>p').html(count_a);
+      $('#add_b>p').html(count_b);
+      $('#add_c>p').html(count_c);
+      console.log('更新成功');
+    }
+  }
+
+}
 
 $(function(){
 
   if( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register('static/scr/serviceworker.js').then(function(registration){
+    navigator.serviceWorker.register('static/scr/serviceworker.js').then( function ( registration ) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(e){
-      console.log('ServiceWorker registration failed: ', e);
+    }).catch( function ( err ) {
+      console.log('ServiceWorker registration failed: ', err);
     });
   }
 
-  // if( storageAvailable('localStorage') ) {
-  //   console.log('can storage');
-  // } else {
-  //   console.log('can\'t storage');
-  // }
-  $('main').click(function(){
-    counter++;
-    $('h1').html(counter);
+  $('#add_a').click(function(){
+    count_a++;
+    $(this).children('p').html(count_a);
+    dbUpdate();
   });
+
+  $('#add_b').click(function(){
+    count_b++;
+    $(this).children('p').html(count_b);
+    dbUpdate();
+  });
+
+  $('#add_c').click(function(){
+    count_c++;
+    $(this).children('p').html(count_c);
+    dbUpdate();
+  });
+
+  $('#reset').click(function(){
+    count_a = 0;
+    count_b = 0;
+    count_c = 0;
+    $('#add_a>p').html(count_a);
+    $('#add_b>p').html(count_b);
+    $('#add_c>p').html(count_c);
+    dbUpdate();
+  });
+
 });
 
-function storageAvailable(type) {
-  try {
-    let storage = window[type],
-              x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
+function dbUpdate(){
+
+  let trans  = db.transaction(storeName, "readwrite"),
+      store  = trans.objectStore(storeName),
+      putReq = store.put({
+        id: 1,
+        action: count_a,
+        buy:    count_b,
+        coin:   count_c
+      });
+
+  putReq.onsuccess = function (event) {
+    console.log('DB UPDATA');
   }
-  catch(e) {
-    return e instanceof DOMException && (
-      // everything except Firefox
-      e.code === 22 ||
-      // Firefox
-      e.code === 1014 ||
-      // test name field too, because code might not be present
-      // everything except Firefox
-      e.name === 'QuotaExceededError' ||
-      // Firefox
-      e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ) &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      storage.length !== 0;
-  }
+
 }
