e31d0f85a4cdc1213f1d375bc35ba0008790390b
wosuke <bloomxxx@me.com>
2019-01-30 14:48:17 +0900

    commit 7

--- static/scr/serviceworker.js
+++ static/scr/serviceworker.js
@@ -9,8 +9,8 @@ let urlsToCache = [
 ];
 
 // インストール処理
-self.addEventListener('install', function(e){
-  e.waitUntil(
+self.addEventListener('install', function(event){
+  event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache){
       return cache.addAll(urlsToCache);
     })
@@ -18,8 +18,8 @@ self.addEventListener('install', function(e){
 });
 
 // リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(e){
-  e.respondWith(
+self.addEventListener('fetch', function(event){
+  event.respondWith(
     caches.match(event.request).then(function(response) {
       return response ? response : fetch(event.request);
     })
