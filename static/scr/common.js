0ac0039098db644429f4ba0053e4e1946397ef6e
wosuke <bloomxxx@me.com>
2019-01-30 11:30:23 +0900

    commit 1

---
+++ static/scr/common.js
@@ -0,0 +1,37 @@
+let counter = 1;
+
+$(function(){
+  // if( storageAvailable('localStorage') ) {
+  //   console.log('can storage');
+  // } else {
+  //   console.log('can\'t storage');
+  // }
+  $('main').click(function(){
+    counter++;
+    $('h1').html(counter);
+  });
+});
+
+function storageAvailable(type) {
+  try {
+    let storage = window[type],
+              x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch(e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ) &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0;
+  }
+}
